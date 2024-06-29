081fe0fc8976509e38aa862d571f1acecc607be9
Kai Singer <kai.singer@outlook.de>
2024-06-29 17:01:26 +0200

    Sessions implementiert

--- rhabitApp/views.py
+++ rhabitApp/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
 
 def home(request):
-  return render(request, 'rhabitApp/home.html', {})
+  if 'user' in request.session:
+    user = request.session['user']
+    return render(request, 'rhabitApp/home.html', { 'name': user })
+  else:
+    return render(request, 'rhabitApp/home.html', { 'name': 'Bitte einloggen!' })
+
+def login(request):
+  request.session['user'] = 'User1'
+  return render(request, 'rhabitApp/home.html', { 'name': 'Login Page' })
+
+def logout(request):
+  user = ''
+  try:
+    user = request.session['user']
+    del request.session['user']
+  except:
+    return render(request, 'rhabitApp/home.html', { 'name': 'Bitte einloggen!' })
+  return render(request, 'rhabitApp/home.html', { 'name': user + ' wurde ausgeloggt!' })
