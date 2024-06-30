130564758247be5df7513884f982e0b830e5c9ff
Kai Singer <kai.singer@outlook.de>
2024-06-30 21:55:32 +0200

    Wrongpwd page

--- rhabitApp/views.py
+++ rhabitApp/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
+import os, json
 
 def user_page(request):
   return render(request, 'rhabitApp/user_page.html', {})
 
 def profile_page(request):
-  if request.method == "POST":
-    return render(request, 'rhabitApp/user_page.html', { 'private': True })
+  return render(request, 'rhabitApp/user_page.html', { 'private': True })
 
 def home_page(request):
   return render(request, 'rhabitApp/home_page.html', {})
@@ -14,7 +14,22 @@ def search_page(request):
   return render(request, 'rhabitApp/search_page.html', {})
 
 def login_page(request):
-  return render(request, 'rhabitApp/login_page.html', {})
+  if len(request.GET) > 0:
+    uname = request.GET.get('uname', '')
+    pwd = request.GET.get('pwd', '')
+    uid = ''
+    for file in os.listdir(f"data/users/"):
+      with open(f"data/users/{file}", "r") as fileContents:
+        fileContents = json.load(fileContents)
+        if fileContents['name'] == uname and fileContents['pwd'] == pwd:
+          uid = os.path.splitext()[0]
+    if uid != '':
+      request.session['uid'] = uid
+      user_page(request)
+    else:
+      return render(request, 'rhabitApp/wrongpwd_page.html', {})
+  else:
+    return render(request, 'rhabitApp/login_page.html', {})
 
 def signup_page(request):
   return render(request, 'rhabitApp/signup_page.html', {})
